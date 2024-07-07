997f07718b4e92f2b24736a1d978fedfc419bbbb
mamapai <mamapai@naver.com>
2024-07-07 22:53:53 +0900

    모달창 및 emaol유효성검사

--- script.js
+++ script.js
@@ -1,19 +1,32 @@
-
-
 document.querySelector('.subscribe_form').addEventListener('submit', function (e) {
   e.preventDefault(); // 기본 폼 제출 막기
   console.log('여기까지 왔어요');
   const email = document.querySelector('.subscribe_form input[name="email"]');
-  const modal = document.querySelector('.modal');
+  const modalContent = document.querySelector('#modal .modalContent');
 
-  // email.checkValidity()로 검사하니 일반텍스트 오류
   // 정규식으로 email패턴 유효검사
   const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
   // 이메일 유효성 검사
   if (emailPattern.test(email.value)) {
-    modal.classList.add('on'); // 모달 열기
+    modalContent.style.display = 'flex'; // 모달 열기
   } else {
     alert('유효한 이메일 주소를 기입해 주세요!');
   }
 });
+
+document.querySelector('.btn-go').addEventListener('click', function (e) {
+  e.preventDefault(); // 기본 링크 동작 막기
+  const modalContent = document.querySelector('#modal .modalContent');
+  modalContent.style.display = 'none'; // 모달 닫기
+});
+
+/* Scroll Top */
+document.querySelector('.scrollTop a').addEventListener('click', function (e) {
+  e.preventDefault();
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth',
+  });
+});
+
