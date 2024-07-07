0b6d93d56510cd0fdc264ac670ad82a143ffa135
mamapai <mamapai@naver.com>
2024-07-08 02:36:50 +0900

    헤더창 fixed

--- script.js
+++ script.js
@@ -22,11 +22,22 @@ document.querySelector('.btn-go').addEventListener('click', function (e) {
 });
 
 /* Scroll Top */
-document.querySelector('.scrollTop a').addEventListener('click', function (e) {
+document.querySelector('.scrollTop').addEventListener('click', function (e) {
   e.preventDefault();
   window.scrollTo({
     top: 0,
     behavior: 'smooth',
   });
+});
+
+/* 스크롤을 50이상 Y축으로 커지면 active추가 */
+window.addEventListener('scroll', function () {
+  const header = document.querySelector('header');
+
+  if (window.scrollY > 70) {
+    header.classList.add('active');
+  } else {
+    header.classList.remove('active');
+  }
 });
 
